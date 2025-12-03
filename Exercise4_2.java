f0e0e59ceba27f3658533da84e41728315a06c52
Sevvalekmmn <250541080@firat.edu.tr>
2025-12-03 21:27:34 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,26 @@
-public class Exercise4_2 {
+Ad Soyad: Şevval Ekmen
+Öğrenci No: 250541080
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
+  public class Exercise4_2 { // 1
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
-    }
+    // Her satirin sonuna çalisma sirasi yorumlarini ekleyin. // 2
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
+    public static void main(String[] args) { // 3
+        zippo("rattle", 13); // 4
+    } // 5
 
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
-        }
-    }
-}
+    public static void baffle(String blimp) { // 6
+        System.out.println(blimp); // 7
+        zippo("ping", -5); // 8
+    } // 12
+
+    public static void zippo(String quince, int flag) { // 9
+        if (flag < 0) { // 10
+            System.out.println(quince + " zoop"); // 11
+        } else { // 13
+            System.out.println("ik"); // 14
+            baffle(quince); // 15
+            System.out.println("boo-wa-ha-ha"); // 16
+        } // 17
+    } // 18
+} // 19
